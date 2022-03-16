b970e811975b552c9b432d6857febe3edd570d93
osoriojl <osoriojl@outlook.com>
2022-03-16 11:30:24 -0400

    added line to String output

--- src/main/java/formatter/Lecture.java
+++ src/main/java/formatter/Lecture.java
@@ -10,9 +10,9 @@ public class Lecture {
         String name1 = String.format("%-15s %-15s %-15s %-15s\n", "Item", "Qty", "Price", "Total");
         String name2 = String.format("%-15s %-15s %-15s %-15s\n", "----", "----", "----", "----");
         String line = String.format("%-15s %-10s %10.2f %15.2f\n", productName, qty, price, total);
-        String output = name1 + name2;
+        String output = name1 + name2 + line;
         System.out.println(output);
-        System.out.println(line);
+
 
     }
 }
